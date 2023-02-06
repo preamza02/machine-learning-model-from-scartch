687857408c5f9f82a12b82f1d57c7e498898197c
ศุภกร คงประพันธ์ <preamza02@gmail.com>
2023-02-06 15:11:52 +0700

    create general class

---
+++ general_class.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class loss_function():
+    def __init__(self):
+        self.name = "loss_funtion"
+
+class MSE(loss_function):
+    def __init__(self):
+        self.name = "MSE"
+
+
+class MAE(loss_function):
+    def __init__(self):
+        self.name = "MSE"
+
